c0ae525d66146781d26f5bdde2f1b72d01c61b58
Fri <changzhongwei3@gmail.com>
2020-05-28 10:57:13 +0800

    update runLoop idle

--- RunLoop-Exercise/demos/RunLoop-Idle/RunLoopDemo0ViewController.m
+++ RunLoop-Exercise/demos/RunLoop-Idle/RunLoopDemo0ViewController.m
@@ -18,86 +18,26 @@ static void runloopCallout(CFRunLoopObserverRef observer, CFRunLoopActivity acti
 //  kCFRunLoopAllActivities = 0x0FFFFFFFU
   switch (activity) {
     case kCFRunLoopEntry:
-      NSLog(@"1-kCFRunLoopEntry");
+      NSLog(@"RunLoop: Entry");
       break;
     case kCFRunLoopBeforeTimers:
-      NSLog(@"1-kCFRunLoopBeforeTimers");
+      NSLog(@"RunLoop: BeforeTimers");
       break;
     case kCFRunLoopBeforeSources:
-      NSLog(@"1-kCFRunLoopBeforeSources");
+      NSLog(@"RunLoop: BeforeSources");
       break;
     case kCFRunLoopBeforeWaiting:
-      NSLog(@"1-kCFRunLoopBeforeWaiting");
+      NSLog(@"RunLoop: BeforeWaiting");
       break;
     case kCFRunLoopAfterWaiting:
-      NSLog(@"1-kCFRunLoopAfterWaiting");
+      NSLog(@"RunLoop: AfterWaiting");
       break;
     case kCFRunLoopExit:
-      NSLog(@"1-kCFRunLoopExit");
+      NSLog(@"RunLoop: Exit");
       break;
     case kCFRunLoopAllActivities:
       break;
   }
-}
-
-static void __runloop_callback(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info){
-  NSString *str;
-  switch (activity) {
-    case kCFRunLoopEntry:
-      str = @"Min Entry";
-      break;
-    case kCFRunLoopBeforeTimers:
-      str = @"Min Before Timersr";
-      break;
-    case kCFRunLoopBeforeSources:
-      str = @"Min Before Sources";
-      break;
-    case kCFRunLoopBeforeWaiting:
-      str = @"Min Before Waiting";
-      break;
-    case kCFRunLoopAfterWaiting:
-      str = @"Min After Waiting";
-      break;
-    case kCFRunLoopExit:
-      str = @"Min Exit";
-      break;
-    case kCFRunLoopAllActivities:
-      str = @"Min AllActivities";
-      break;
-    default:
-      break;
-  }
-  NSLog(@"current activity:%@",str);
-}
-
-static void __runloop_before_waiting_callback(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info) {
-  NSString *str;
-  switch (activity) {
-    case kCFRunLoopEntry:
-      str = @"Max Entry";
-      break;
-    case kCFRunLoopBeforeTimers:
-      str = @"Max Before Timersr";
-      break;
-    case kCFRunLoopBeforeSources:
-      str = @"Max Before Sources";
-      break;
-    case kCFRunLoopBeforeWaiting:
-      str = @"Max Before Waiting";
-      break;
-    case kCFRunLoopAfterWaiting:
-      str = @"Max After Waiting";
-      break;
-    case kCFRunLoopExit:
-      str = @"Max Exit";
-      break;
-    case kCFRunLoopAllActivities:
-      str = @"Max AllActivities";
-      break;
-    default:
-      break;
-  }
-  NSLog(@"current activity:%@",str);
 }
 
 @interface RunLoopDemo0ViewController ()
@@ -110,63 +50,30 @@ static int count = 0;
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.view.backgroundColor = [UIColor whiteColor];
-  
+
   [self testRunLoopObserver];
 }
 
-- (void)testRunloopIdle{
-  CFRunLoopRef rl = CFRunLoopGetCurrent();
-  CFRunLoopObserverRef o1 = CFRunLoopObserverCreate(kCFAllocatorDefault, kCFRunLoopBeforeWaiting, true, 0, runloopCallout, nil);
-  CFRunLoopObserverRef o2 = CFRunLoopObserverCreateWithHandler( kCFAllocatorDefault, kCFRunLoopBeforeWaiting, true, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
-    if (count == 3) {
-      CFRunLoopRemoveObserver(rl, o1, kCFRunLoopDefaultMode);
-      CFRunLoopRemoveObserver(rl, observer, kCFRunLoopDefaultMode);
-      CFRelease(o1);
-      CFRelease(observer);
-      return;
-    }
-    
-    [self performSelector:@selector(runloopObserver:) onThread:NSThread.mainThread withObject:@(count) waitUntilDone:false modes:@[NSDefaultRunLoopMode]];
-    ++count;
-    switch (activity) {
-      case kCFRunLoopEntry:
-        NSLog(@"2-kCFRunLoopEntry");
-        break;
-      case kCFRunLoopBeforeTimers:
-        NSLog(@"2-kCFRunLoopBeforeTimers");
-        break;
-      case kCFRunLoopBeforeSources:
-        NSLog(@"2-kCFRunLoopBeforeSources");
-        break;
-      case kCFRunLoopBeforeWaiting:
-        NSLog(@"2-kCFRunLoopBeforeWaiting");
-        break;
-      case kCFRunLoopAfterWaiting:
-        NSLog(@"2-kCFRunLoopAfterWaiting");
-        break;
-      case kCFRunLoopExit:
-        NSLog(@"2-kCFRunLoopExit");
-        break;
-      case kCFRunLoopAllActivities:
-        break;
-    }
-  });
-  
-  CFRunLoopAddObserver(rl, o1, kCFRunLoopDefaultMode);
-  CFRunLoopAddObserver(rl, o2, kCFRunLoopDefaultMode);
-}
-
 - (void) runloopObserver:(id) value {
-  NSLog(@"2333--%@", value);
+  NSLog(@"beforeWaiting: doing --%@", value);
 }
-
 
 - (void)testRunLoopObserver{
   CFRunLoopObserverContext ctx = { 0, (__bridge void *)self, NULL, NULL };
-  CFRunLoopObserverRef allActivitiesObserver = CFRunLoopObserverCreate(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, NSIntegerMin, &__runloop_callback, &ctx);
+  CFRunLoopObserverRef allActivitiesObserver = CFRunLoopObserverCreate(kCFAllocatorDefault, kCFRunLoopAllActivities, YES, NSIntegerMin, &runloopCallout, &ctx);
   CFRunLoopAddObserver(CFRunLoopGetCurrent(), allActivitiesObserver, kCFRunLoopCommonModes);
   
-  CFRunLoopObserverRef beforeWaitingObserver = CFRunLoopObserverCreate(kCFAllocatorDefault, kCFRunLoopBeforeWaiting, YES, NSIntegerMax, &__runloop_before_waiting_callback, &ctx);
+  CFRunLoopRef rl = CFRunLoopGetCurrent();
+  CFRunLoopObserverRef beforeWaitingObserver = CFRunLoopObserverCreateWithHandler( kCFAllocatorDefault, kCFRunLoopBeforeWaiting, true, 0, ^(CFRunLoopObserverRef observer, CFRunLoopActivity activity) {
+    if (count == 3) {
+      CFRunLoopRemoveObserver(rl, observer, kCFRunLoopDefaultMode);
+      CFRelease(observer);
+      return;
+    }
+    NSLog(@"observer beforeWaiting");
+    [self performSelector:@selector(runloopObserver:) onThread:NSThread.mainThread withObject:@(count) waitUntilDone:false modes:@[NSDefaultRunLoopMode]];
+    ++count;
+  });
   CFRunLoopAddObserver(CFRunLoopGetCurrent(), beforeWaitingObserver, kCFRunLoopCommonModes);
 }
 @end
